d28f253796f336d1b503a8db37ad705c1f890f42
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 18:34:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(item, price, qnty = 1)
     item_total = price * qnty
-    @items[#{item}] = (item_total)
+    @items["#{item}" => (item_total)]
     @total += item_total
     @last_transaction_amount = item_total
     @total
