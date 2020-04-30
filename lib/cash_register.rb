01a5fe35a1452f9a35b200705510651024d37338
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 15:59:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 class CashRegister
   
-  attr_accessor :total, :employee_discount
+  attr_accessor :total, :employee_discount, :items
   
   def initialize
     @total = 0.00
-    @employee_discount = nil
+    @employee_discount = discount
   end
   
   def apply_discount
