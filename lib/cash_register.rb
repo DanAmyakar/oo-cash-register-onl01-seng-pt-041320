c5285094dcd64af501c0277f5089972c2279b5dc
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 18:41:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,12 +6,12 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0.00           # Sets total to zero, should display current total
     @discount = discount    # Sets a discount value if there is one
-    @items = {}             # Store an array of items (might make this a hash)
+    @items = []             # Store an array of items (might make this a hash)
   end
   
   def add_item(item, price, qnty = 1)
     item_total = price * qnty
-    @items["#{item}" => (item_total)]
+    @items << [item]
     @total += item_total
     @last_transaction_amount = item_total
     @total
