56da9d6b9a74cdb295c305cfe6ca12308840a5b4
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 17:28:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,52 +2,12 @@ class CashRegister
   
   attr_accessor :total, :discount, :items, :last_transaction_amount
   
+  # Upon init opts a discount
   def initialize(discount = 0)
-    @total = 0.00
-    @discount = discount
-    @items = []
+    @total = 0.00           # Sets total to zero, should display current total
+    @discount = discount    # Sets a discount value if there is one
+    @items = []             # Store an array of items (might make this a hash)
   end
   
-  def add_item(title, price, qnty = 1)
-    i = 0
-    
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  def add_item(title, price, qnty = 1)
-    if qnty > 1
-      i = 0
-      while i < qnty
-        @items << title
-        i += 1
-      end
-    else
-      @items << title
-    end
-    @last_transaction_amount += (price * qnty)
-    @total += @last_transaction_amount
-    @total
-  end
-    
-  
-  def apply_discount()
-    if @discount > 0
-      @dicount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After your discount your total is $#{@total}"
-    else
-      "There is no discount to apply."
-    end
-  end
-  
-  def void_last_transaction
-    @total = 0.0
-  end
   
 end
