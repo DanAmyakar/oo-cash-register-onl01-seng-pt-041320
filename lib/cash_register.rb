c159dbe6b373c40a27710eb7f09db5596011cbb6
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 18:06:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,9 +6,9 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0.00           # Sets total to zero, should display current total
     @discount = discount    # Sets a discount value if there is one
-    @items = []             # Store an array of items (might make this a hash)
+    @items = {}             # Store an array of items (might make this a hash)
   end
   
   def void_last_transaction
-    @items.last.pop
+    @items
 end
